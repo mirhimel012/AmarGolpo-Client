20c08c2fdda890df8cb64d1695f63c2298432e45
himelmia535 <himelmia535@gmail.com>
2024-05-10 21:08:56 +0600

    theme and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,73 @@
+import { Link, NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+  const navlinks = <>
+  <li><NavLink to="/">Home</NavLink></li>
+  <li><NavLink to="/addbook">Add Book</NavLink></li>
+  <li><NavLink to="/allbooks">All Books</NavLink></li>
+  <li><NavLink to="/borrowedbooks">Borrowed Books</NavLink></li>
+</>
+
+const [theme, setTheme] = useState('light')
+    // update state on toggle
+    const handleToggle = (e) => {
+        if (e.target.checked) {
+            setTheme('synthwave')
+        } else {
+            setTheme('light')
+        }
+    }
+    
+    // set theme state in localStorage on mount & also update localStorage on state change
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+        const localTheme = localStorage.getItem('theme')
+    
+        // add custom data-theme attribute
+        document.querySelector('html').setAttribute('data-theme', localTheme)
+      }, [theme])
+
     return (
         <div>
-            <h2 className="text-4xl">This is Nav</h2>
+            <div className="navbar bg-base-100 shadow-lg px-4 sm:px-8 fixed z-10">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        {navlinks}
+      </ul>
+    </div>
+    <div className="flex gap-1">
+        <Link to="/"><img className="w-14 h-14" src="https://i.ibb.co/4dDWk3Q/books.png" alt="" /></Link>
+        <h2 className="text-2xl font-bold items-center md:text-4xl">Spellbound</h2>
+    </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 text-xl">
+      {navlinks}
+    </ul>
+  </div>
+  <div className="navbar-end gap-3">
+  <label className="cursor-pointer grid place-items-center">
+  <input onChange={handleToggle} type="checkbox" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
+  <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+  <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+</label>
+<lavel>
+<div>
+            <NavLink to="/login" className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+<span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+<span className="relative">Login</span>
+</NavLink>
         </div>
+</lavel>
+  </div>
+      
+  </div>
+</div>
     );
 };
 
