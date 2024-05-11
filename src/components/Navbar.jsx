37fa6343a3d1492bc5c261d68b4200b68a061a36
himelmia535 <himelmia535@gmail.com>
2024-05-11 12:34:22 +0600

    navbar profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const Navbar = () => {
+  const {logout, user} = useAuth()
   const navlinks = <>
   <li><NavLink to="/">Home</NavLink></li>
   <li><NavLink to="/addbook">Add Book</NavLink></li>
@@ -56,14 +58,33 @@ const [theme, setTheme] = useState('light')
   <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
   <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 </label>
-<lavel>
-<div>
+
+{
+    user? <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar relative" >
+        <div className="w-12 rounded-full">
+            <img src={user?.photoURL || "https://i.ibb.co/WsZ4wFT/Rectangle-7.png"} alt="" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <button className="btn btn-sm btn-ghost">{user?.displayName || "Not Found"}</button>
+        </li>
+        <li>
+          <button onClick={logout} className="btn btn-sm btn-ghost hover:text-red-500">Logout</button>
+        </li>
+      </ul>
+      </div>
+      :
+      <div className="flex gap-2">
+        <div>
             <NavLink to="/login" className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
 <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
 <span className="relative">Login</span>
 </NavLink>
         </div>
-</lavel>
+      </div>
+   }
   </div>
       
   </div>
