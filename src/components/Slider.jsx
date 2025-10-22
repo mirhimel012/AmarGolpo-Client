c8de0fd769b7e71f70b914892f5cc0d972a494b4
mirhimel012 <himelmir012@gmail.com>
2025-10-22 21:32:26 +0600

    update slider to modern elegant version

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,42 +7,28 @@ const Slider = () => {
     const timer = setTimeout(() => {
       const nextSlide = currentSlide === 4 ? 1 : currentSlide + 1;
       setCurrentSlide(nextSlide);
-    }, 5000); // Change slide every 5 seconds
+    }, 3000); // 1000 milliseconds is 1 second
 
     return () => clearTimeout(timer);
   }, [currentSlide]);
 
-  const handlePrevSlide = () => {
-    const prevSlide = currentSlide === 1 ? 4 : currentSlide - 1;
-    setCurrentSlide(prevSlide);
-  };
-
-  const handleNextSlide = () => {
-    const nextSlide = currentSlide === 4 ? 1 : currentSlide + 1;
-    setCurrentSlide(nextSlide);
-  };
 
   return (
-    <div>
-      <div className="carousel w-full h-svh">
-        <div id={`slide1`} className={`carousel-item relative w-full h-fit ${currentSlide === 1 ? 'block' : 'hidden'}`}>
-            <img src="https://i.ibb.co/KDNnF8s/pexels-element5-1370298.jpg" className="w-full h-fit" />        
-        </div>
-        <div id={`slide2`} className={`carousel-item relative w-full h-fit ${currentSlide === 2 ? 'block' : 'hidden'}`}>
-            <img src="https://i.ibb.co/xJMTfGq/pexels-pixabay-256455.jpg" className="w-full h-fit"/>
-        </div>
-        <div id={`slide3`} className={`carousel-item relative w-full h-fit ${currentSlide === 3 ? 'block' : 'hidden'}`}>
-            <img src="https://i.ibb.co/DWDY1v8/reading-8575569-1280.jpg" className="w-full h-fit" />
-        </div>
-        <div id={`slide4`} className={`carousel-item relative w-full h-fit ${currentSlide === 4 ? 'block' : 'hidden'}`}>
-            <img src="https://i.ibb.co/1m8xxhN/pexels-pixabay-159775.jpg" className="w-full h-fit" />
-        </div>
-      </div>
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <button onClick={handlePrevSlide} className="btn btn-circle">❮</button>
-        <button onClick={handleNextSlide} className="btn btn-circle">❯</button>
-      </div>
-    </div>
+    <div className="carousel w-full h-[620px] relative overflow-hidden">
+  <div id="slide1" className={`carousel-item absolute w-full h-full ${currentSlide === 1 ? 'block' : 'hidden'}`}>
+    <img src="https://i.ibb.co.com/KDNnF8s/pexels-element5-1370298.jpg" className="w-full h-full object-cover" />
+  </div>
+  <div id="slide2" className={`carousel-item absolute w-full h-full ${currentSlide === 2 ? 'block' : 'hidden'}`}>
+    <img src="https://i.ibb.co.com/j6tHrML/dog-7575389-1280.jpg" className="w-full h-full object-cover" />
+  </div>
+  <div id="slide3" className={`carousel-item absolute w-full h-full ${currentSlide === 3 ? 'block' : 'hidden'}`}>
+    <img src="https://i.ibb.co.com/xJMTfGq/pexels-pixabay-256455.jpg" className="w-full h-full object-cover" />
+  </div>
+  <div id="slide4" className={`carousel-item absolute w-full h-full ${currentSlide === 4 ? 'block' : 'hidden'}`}>
+    <img src="https://i.ibb.co.com/DWDY1v8/reading-8575569-1280.jpg" className="w-full h-full object-cover" />
+  </div>
+</div>
+
   );
 };
 
