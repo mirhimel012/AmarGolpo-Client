5bdc00f1791711b3a9a28b17caa2bde1db06c619
mirhimel012 <himelmir012@gmail.com>
2025-10-24 20:12:39 +0600

    Firebase Updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDd1NlpxpPMyfzegHGDljkqqJFnwzIDzBM",
-  authDomain: "spellbound-library.firebaseapp.com",
-  projectId: "spellbound-library",
-  storageBucket: "spellbound-library.appspot.com",
-  messagingSenderId: "936805679495",
-  appId: "1:936805679495:web:0b527509a0647daa955322"
+  apiKey: "AIzaSyDez4haCmSg8G0qqDYUFpHXbH7PHdKvAC0",
+  authDomain: "amargolpofire.firebaseapp.com",
+  projectId: "amargolpofire",
+  storageBucket: "amargolpofire.firebasestorage.app",
+  messagingSenderId: "343354545205",
+  appId: "1:343354545205:web:fb0f16354e4282e60f8d7c",
+  measurementId: "G-QPWHLCPPRD"
 };
 
 // Initialize Firebase
